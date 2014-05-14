23cb857d65b6fb484b7dc68a9d87c69edbe24e31
Ijwu <hmfarran1@hawkmail.hfcc.edu>
2014-05-13 22:53:57 -0400

    Edited status message for formatting.

--- MusicPlayer/Music/Song.cs
+++ MusicPlayer/Music/Song.cs
@@ -59,7 +59,7 @@ namespace MusicPlayer.Music
 			currentPlayTime += (int)delta;
 			var compl = Math.Round((songLength - notesToPlay.Count) / (songLength * 1.0), 2);
 			var padding = Math.Floor(Math.Log10(songLength) + 1);
-			player.Player.SendData(PacketTypes.Status, String.Format("Currently Playing: {0} > {1, " + padding + "}/{2, " + padding + "}({3})" +
+			player.Player.SendData(PacketTypes.Status, String.Format("Currently Playing: {0} > {1, " + padding + "}/{2, " + padding + "} ({3}%)" +
 			                                                         "                                     ", 
 																	 Name, (songLength - notesToPlay.Count), songLength, (int)Math.Round(compl * 100.0)), 1);
 
